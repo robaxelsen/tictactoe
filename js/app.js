21362c95b63d41799e2a69de47edf360a303a046
Robert Axelsen <rob@rob.ee>
2018-01-13 21:52:11 +0100

    Do complete rewrite and add minimal features

---
+++ js/app.js
@@ -0,0 +1,67 @@
+var cell0 = document.getElementById('0');
+var cell1 = document.getElementById('1');
+var cell2 = document.getElementById('2');
+var cell3 = document.getElementById('3');
+var cell4 = document.getElementById('4');
+var cell5 = document.getElementById('5');
+var cell6 = document.getElementById('6');
+var cell7 = document.getElementById('7');
+var cell8 = document.getElementById('8');
+var p1 = document.getElementById('player-1');
+var p2 = document.getElementById('player-2');
+var turn = 1;
+
+var states = [
+    // [0], [1], [2]
+    cell0, cell1, cell2,
+    // [3], [4], [5]
+    cell3, cell4, cell5,
+    // [6], [7], [8]
+    cell6, cell7, cell8
+];
+
+var clickCell = (id) => {
+    if (states[id].innerHTML === "") {
+        if (turn % 2 === 0) {
+            states[id].innerHTML = "O"
+        } else {
+            states[id].innerHTML = "X"
+        }
+    };
+    checkWin('X');
+    checkWin('O');
+    turn ++;
+    changePlayer();
+};
+
+var changePlayer = () => {
+    if (turn % 2 === 0) {
+        p1.style.display = "block";
+        p2.style.display = "none";
+    } else {
+        p2.style.display = "block";
+        p1.style.display = "none";
+    };
+};
+
+var checkWin = (symbol) => {
+    if (states[0].innerHTML === symbol && states[1].innerHTML === symbol && states[2].innerHTML === symbol) {
+        alert(`${symbol} WON THE GAME`);
+    } else if (states[3].innerHTML === symbol && states[4].innerHTML === symbol && states[5].innerHTML === symbol) {
+        alert(`${symbol} WON THE GAME`);
+    } else if (states[6].innerHTML === symbol && states[7].innerHTML === symbol && states[8].innerHTML === symbol) {
+        alert(`${symbol} WON THE GAME`);
+    } else if (states[0].innerHTML === symbol && states[4].innerHTML === symbol && states[8].innerHTML === symbol) {
+        alert(`${symbol} WON THE GAME`);
+    } else if (states[2].innerHTML === symbol && states[4].innerHTML === symbol && states[6].innerHTML === symbol) {
+        alert(`${symbol} WON THE GAME`);
+    } else if (states[3].innerHTML === symbol && states[4].innerHTML === symbol && states[5].innerHTML === symbol) {
+        alert(`${symbol} WON THE GAME`);
+    } else if (states[0].innerHTML === symbol && states[3].innerHTML === symbol && states[6].innerHTML === symbol) {
+        alert(`${symbol} WON THE GAME`);
+    } else if (states[1].innerHTML === symbol && states[4].innerHTML === symbol && states[7].innerHTML === symbol) {
+        alert(`${symbol} WON THE GAME`);
+    } else if (states[2].innerHTML === symbol && states[5].innerHTML === symbol && states[8].innerHTML === symbol) {
+        alert(`${symbol} WON THE GAME`);
+    };
+};
