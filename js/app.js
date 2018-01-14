28d87a7253c8adfe2151ecdcaf3c33be4372bcfb
Robert Axelsen <rob@rob.ee>
2018-01-14 16:09:32 +0100

    Complete final cleanup and styling changes

--- js/app.js
+++ js/app.js
@@ -1,13 +1,3 @@
-// DONE: 1. My game will reset as soon as it's over so I can play again
-// DONE: a. set all cells to innerHTML ""
-// DONE: b. set turn to 1
-// DONE: c. set player title to visible/block
-// DONE: 2. I can play a game of Tic Tac Toe with the computer
-// DONE: a. find total number of empty cells, and pick a random cell to populate
-// DONE: 3. I can choose whether I want to play as X or O
-// TODO: 4. Styling
-// TODO: 5. Final testing and cleanup
-
 var cell0 = document.getElementById('0');
 var cell1 = document.getElementById('1');
 var cell2 = document.getElementById('2');
@@ -17,8 +7,10 @@ var cell5 = document.getElementById('5');
 var cell6 = document.getElementById('6');
 var cell7 = document.getElementById('7');
 var cell8 = document.getElementById('8');
-var statusX = document.getElementById('as-x');
-var statusO = document.getElementById('as-o');
+var symbolStatus = document.getElementById('symbol');
+var symbolButton = document.getElementById('symbol-button');
+var wonX = document.getElementById('x-won');
+var wonO = document.getElementById('o-won');
 var turn = 1;
 var gameOver = false;
 var playerSymbol = "X";
@@ -35,7 +27,6 @@ var states = [
 var computerPlays = () => {
     var computerToken = playerSymbol === "X" ? "O" : "X";
     var emptyCells = [];
-    //var playerSymbol;
     var randomCellIndex;
     var randomCell;
     states.forEach((state, index) => {
@@ -44,71 +35,77 @@ var computerPlays = () => {
         };
     });
     randomCellIndex = Math.floor(Math.random() * ((emptyCells.length - 1) - 0 + 1)) + 0;
-    states[parseInt(emptyCells[randomCellIndex])].innerHTML = computerToken;
-    checkWin(computerToken, 'COMPUTER');
+	computerPickedCell = states[parseInt(emptyCells[randomCellIndex])] || null;
+	console.log(computerPickedCell);
+	if (!!computerPickedCell) {
+    	computerPickedCell.innerHTML = computerToken;
+		turn ++;
+		checkWin(computerToken, 'COMPUTER');
+	};
 }
 
 var resetGame = () => {
     for (var id=0;id < 9;id++) {
         states[id].innerHTML = "";
     };
-    statusX.style.display = "inline-block";
-    statusO.style.display = "none";
+    wonX.style.display = "none";
+    wonO.style.display = "none";
+    symbolButton.style.opacity = "1";
     turn = 1;
 };
 
 var clickCell = (id) => {
+    if (turn) {
+        symbolButton.style.opacity = "0";
+    };
     if (states[id].innerHTML === "") {
         states[id].innerHTML = playerSymbol;
-        checkWin(playerSymbol, 'YOU');
         turn ++;
+        checkWin(playerSymbol, 'YOU');
         computerPlays();
     };
     if (gameOver) {
-        resetGame();
+		window.setTimeout(resetGame, 2000);
         gameOver = false;
     };
 };
 
-var changeToX = () => {
-    playerSymbol = "X";
-    statusX.style.display = "inline-block";
-    statusO.style.display = "none";
-};
-
-var changeToO = () => {
-    playerSymbol = "O";
-    statusO.style.display = "inline-block";
-    statusX.style.display = "none";
+var changeSymbol = () => {
+    var newSymbol = playerSymbol === "X" ? "O" : "X";
+    playerSymbol = newSymbol;
+    symbolStatus.innerHTML = `YOUR SYMBOL: ${newSymbol}`;
 };
 
 var checkWin = (symbol, player) => {
     if (states[0].innerHTML === symbol && states[1].innerHTML === symbol && states[2].innerHTML === symbol) {
         gameOver = true;
-        alert(`${player} WON THE GAME`);
+        document.getElementById(`${symbol.toLowerCase()}-won`).style.display = "inline-block";
     } else if (states[3].innerHTML === symbol && states[4].innerHTML === symbol && states[5].innerHTML === symbol) {
         gameOver = true;
-        alert(`${player} WON THE GAME`);
+        document.getElementById(`${symbol.toLowerCase()}-won`).style.display = "inline-block";
     } else if (states[6].innerHTML === symbol && states[7].innerHTML === symbol && states[8].innerHTML === symbol) {
         gameOver = true;
-        alert(`${player} WON THE GAME`);
+        document.getElementById(`${symbol.toLowerCase()}-won`).style.display = "inline-block";
     } else if (states[0].innerHTML === symbol && states[4].innerHTML === symbol && states[8].innerHTML === symbol) {
         gameOver = true;
-        alert(`${player} WON THE GAME`);
+        document.getElementById(`${symbol.toLowerCase()}-won`).style.display = "inline-block";
     } else if (states[2].innerHTML === symbol && states[4].innerHTML === symbol && states[6].innerHTML === symbol) {
         gameOver = true;
-        alert(`${player} WON THE GAME`);
+        document.getElementById(`${symbol.toLowerCase()}-won`).style.display = "inline-block";
     } else if (states[3].innerHTML === symbol && states[4].innerHTML === symbol && states[5].innerHTML === symbol) {
         gameOver = true;
-        alert(`${player} WON THE GAME`);
+        document.getElementById(`${symbol.toLowerCase()}-won`).style.display = "inline-block";
     } else if (states[0].innerHTML === symbol && states[3].innerHTML === symbol && states[6].innerHTML === symbol) {
         gameOver = true;
-        alert(`${player} WON THE GAME`);
+        document.getElementById(`${symbol.toLowerCase()}-won`).style.display = "inline-block";
     } else if (states[1].innerHTML === symbol && states[4].innerHTML === symbol && states[7].innerHTML === symbol) {
         gameOver = true;
-        alert(`${player} WON THE GAME`);
+        document.getElementById(`${symbol.toLowerCase()}-won`).style.display = "inline-block";
     } else if (states[2].innerHTML === symbol && states[5].innerHTML === symbol && states[8].innerHTML === symbol) {
         gameOver = true;
-        alert(`${player} WON THE GAME`);
+        document.getElementById(`${symbol.toLowerCase()}-won`).style.display = "inline-block";
     };
+	if (turn > 9) {
+		gameOver = true;
+	};
 };
